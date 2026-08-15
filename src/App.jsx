cc3da63d73dafc9f43ebd5d84e90fe736d8ea1ef
Aviya Ben David <aviyabdavid@gmail.com>
2026-08-16 00:47:39 +0300

    Permanent Background

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage.jsx'
+import Background from './components/Background.jsx'
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<LandingPage />} />
-        </Routes>
+        <div className="app">
+            <Background />
+
+            <div className="app-content">
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                </Routes>
+            </div>
+        </div>
     )
 }
-
 export default App
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,15 +1,11 @@
 import './LandingPage.css'
-import Background from '../components/Background.jsx'
 
 function LandingPage() {
     return (
         <div className="landing-page">
-            <Background />
-
             <div className="landing-content">
                 <h1>מערכת לקביעת תורים</h1>
-
-                <button>קביעת תור</button>
+                <button>קביעת תורים</button>
                 <button>מחירון</button>
             </div>
         </div>
